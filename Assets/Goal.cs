79e640980b7c3146dde523e5a66d6555d8884e8d
HideakiTouhara <hideee6723@gmail.com>
2017-06-20 20:42:29 +0900

    create goal object and gameover object

---
+++ Assets/Goal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+	[SerializeField] Clear clear;
+
+	void OnTriggerEnter(Collider other) {
+		clear.ClearAppear();
+	}
+}
