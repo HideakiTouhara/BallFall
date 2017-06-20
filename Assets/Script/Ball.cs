6452f8f5fb01019d61622f9f79e6396591abfad5
kyoh <stayheresilence@gmail.com>
2017-06-20 20:04:45 +0900

    set ball

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 	void FixedUpdate ()
 	{
-	    var force = new Vector3(Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"))*20f;
+	    var force = new Vector3(Input.GetAxis("Vertical"), 0f, -Input.GetAxis("Horizontal"))*20f;
 	    GetComponent<Rigidbody>().AddForce(force);
 	}
 }
